5f57829b141806b8cdd1d42b147053f7a7bd46a3
dres0 <dres0@andresrg.localdomain>
2022-03-30 20:17:53 -0300

    Finished: filtro_procesos.rb

---
+++ filtro_procesos.rb
@@ -0,0 +1,21 @@
+# Se necesita crear un programa llamado `filtro_procesos.rb` que lea un archivo que tiene
+# datos por línea. Estos datos representan la cantidad de milisegundos que demoran en
+# terminar algunos procesos del sistema operativo.
+
+def data_filter(file = 'procesos.data', filter)
+    #abrimos y leemos el archivo
+    data = open(file).readlines #array donde cada elemento es una linea del archivo
+    #transformarlo a numero para comparar
+    filter = filter.to_i
+    data = data.map do |element|
+        element.to_i
+    end
+    #los valores deben ser mayores a filter para ser seleccionados
+    filtered_array = data.select do |element|
+        element > filter
+    end
+    # debemos escribirlos en un archivo que creamos
+    File.write('procesos_filtrados.data', filtered_array.join("\n"))
+end
+
+data_filter(ARGV[0])
